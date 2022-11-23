0928a047669786b744967ed3550b9d09f0affbdb
Prerana <dahiwadeprerana@gmail.com>
2022-11-23 19:39:04 +0530

    [prerana]Add:BubbleSort

--- AlgorithmsAndSearches/Program.cs
+++ AlgorithmsAndSearches/Program.cs
@@ -7,7 +7,7 @@
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("select option\n 1.Binary Search\n 2.InsertionSort");
+                Console.WriteLine("select option\n 1.Binary Search\n 2.InsertionSort \n 3.BubbleSort");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -21,6 +21,12 @@
                         int[] binaryarr = { 1, 2, 4, 5, 7, 9 };
                         insertionSort.Sort(binaryarr);
                         break;
+                    case 3:
+                        BubbleSort bubbleSort = new BubbleSort();
+                        int[] arr= {5,67,23, 5, 7, 9 };
+                        bubbleSort.Sort(arr);
+                        break;
+
 
 
 
