b5546b84748348e62a7db1310c2a32bfc3f850ca
Prerana <dahiwadeprerana@gmail.com>
2022-11-23 19:09:59 +0530

    [prerana]Add:BinarySearch

---
+++ AlgorithmsAndSearches/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace AlgorithmsAndSearches
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("select option\n 1.Binary Search\n");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        BinarySearch binarySearch = new BinarySearch();
+                        binarySearch.ReadTextFile(@"D:\217.NetBatch\Algorithm_Programs\AlgorithmsAndSearches\binarysearch.txt");
+                        binarySearch.BinarySearchOperation("We");
+                        break;
+
+
+                }
+
+            }
+        }
+    }
+}
