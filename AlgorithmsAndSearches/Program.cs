15ce871f94dfba43910b7d47c4cf391310c2e098
Prerana <dahiwadeprerana@gmail.com>
2022-11-23 19:24:09 +0530

    [prerana]Add:InsertionSort

--- AlgorithmsAndSearches/Program.cs
+++ AlgorithmsAndSearches/Program.cs
@@ -7,7 +7,7 @@
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("select option\n 1.Binary Search\n");
+                Console.WriteLine("select option\n 1.Binary Search\n 2.InsertionSort");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -16,6 +16,13 @@
                         binarySearch.ReadTextFile(@"D:\217.NetBatch\Algorithm_Programs\AlgorithmsAndSearches\binarysearch.txt");
                         binarySearch.BinarySearchOperation("We");
                         break;
+                    case 2:
+                        InsertionSort insertionSort = new InsertionSort();
+                        int[] binaryarr = { 1, 2, 4, 5, 7, 9 };
+                        insertionSort.Sort(binaryarr);
+                        break;
+
+
 
 
                 }
