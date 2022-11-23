14be4d60d4c05e6a3f5003ceb7d1da2fa88c15ae
Prerana <dahiwadeprerana@gmail.com>
2022-11-23 20:39:21 +0530

    [prerana]Add:MessageThroughUserInput

--- AlgorithmsAndSearches/Program.cs
+++ AlgorithmsAndSearches/Program.cs
@@ -1,4 +1,5 @@
 ﻿using static AlgorithmsAndSearches.UseAnagram;
+using static AlgorithmsAndSearches.UserInput;
 
 namespace AlgorithmsAndSearches
 {
@@ -9,7 +10,7 @@ namespace AlgorithmsAndSearches
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("select option\n 1.Binary Search\n 2.InsertionSort \n 3.BubbleSort \n 4.Anagram");
+                Console.WriteLine("select option\n 1.Binary Search\n 2.InsertionSort \n 3.BubbleSort \n 4.Anagram \n 5.UserInput");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -25,22 +26,22 @@ namespace AlgorithmsAndSearches
                         break;
                     case 3:
                         BubbleSort bubbleSort = new BubbleSort();
-                        int[] arr= {5,67,23, 5, 7, 9 };
+                        int[] arr = { 5, 67, 23, 5, 7, 9 };
                         bubbleSort.Sort(arr);
                         break;
                     case 4:
                         Anagram check = new Anagram();
                         check.CheckAnagram("heart", "earth");
                         break;
-
-
-
-
-
-
+                    case 5:
+                        MessageDemonstration replace = new MessageDemonstration();
+                        replace.ReplaceString();
+                        break;
                 }
-
             }
+
         }
+
     }
 }
+    
---
+++ AlgorithmsAndSearches/UserInput.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsAndSearches
+{
+    internal class UserInput
+    {
+        public class MessageDemonstration
+    {
+        public void ReplaceString()
+         {
+            string input = "Hello << name >> We have your fullname as << fullname >> in our system. your contact number is 91 - xxxxxxxxxx.Please,let us know in case of any clarification.Thank you BridgeLabz" ;
+            Console.WriteLine("Enter your name");
+            string name = Console.ReadLine();
+            input = input.Replace("<< name >>", name);
+
+            Console.WriteLine("Enter your Full name");
+            string fullname = Console.ReadLine();
+            input = input.Replace("<< fullname >>",fullname);
+
+            Console.WriteLine("Enter your Phone Number");
+            string PhoneNo = Console.ReadLine();
+            input = input.Replace("xxxxxxxxxx", PhoneNo);
+
+                Console.WriteLine(input);
+        }
+         
+        }
+    
+    }
+   
+}
+
